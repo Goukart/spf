f6d80cbc08c6791e03b6b75c441f422aa67645c4
SirTunahead <ralf.speer@hs-furtwangen.de>
2022-10-06 23:44:26 +0200

    right animation for right direction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("spf")]
+
 async fn main() {
     let mut player_x = 100.0;
     let mut player_y = 100.0;
@@ -11,16 +12,24 @@ async fn main() {
         Image::from_file_with_format(include_bytes!("../resource/playersprite.png"), None);
 
     let mut time = get_time();
-    let init_time = time;
+
+    let mut right = true;
+    let mut left = false;
+    
     loop {
         clear_background(BLACK);
         let delta = get_frame_time();
 
         if is_key_down(KeyCode::Right) {
             player_x += 100.0 * delta;
+            right = false;
+            left = true;
         }
         if is_key_down(KeyCode::Left) {
             player_x -= 100.0 * delta;
+            right = true;
+            left = false;
+            
         }
         if is_key_down(KeyCode::Down) {
             player_y += 100.0 * delta;
@@ -28,26 +37,43 @@ async fn main() {
         if is_key_down(KeyCode::Up) {
             player_y -= 100.0 * delta;
         }
+
         if time  <= 0.1 {
+            if right {
             img_x = 40.0;
+            }
+            if left {
+            img_x = 520.0;
+            }
         }
         if time > 0.2 && time < 0.3 {
+           if right {
             img_x = 200.0;
+            }
+
+            if left {
+                img_x = 680.0;
+            }
         }        
 
         if time > 0.3 { 
+            if right {
             img_x = 360.0;
+            }
+            if left {
+                img_x = 840.0;
+            }
+
         }
 
         time = get_time();
         time = time % 0.4;
-        
-        println!("{}, {}", time, init_time);
-        let rect = Rect::new(img_x, 0.0, 80.0, 160.0);
-        let subfren = fren_img.sub_image(rect);
 
-        let fren_texture = Texture2D::from_image(&subfren);
-        fren_texture.update(&subfren);
+        let rect = Rect::new(img_x, 0.0, 80.0, 160.0);
+        let subimage = fren_img.sub_image(rect);
+
+        let fren_texture = Texture2D::from_image(&subimage);
+        fren_texture.update(&subimage);
         draw_texture(fren_texture, player_x, player_y, WHITE);
 
         next_frame().await
