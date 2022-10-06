170068733c8809b9be675bddebee621c2b068d07
Goukart <benjaminherzberger@web.de>
2022-10-06 17:28:23 +0200

    macroquad init

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,16 @@
-#[macro_use]
-extern crate glium;
+use macroquad::prelude::*;
 
-#[path = "/home/ralf/Documents/Github/spf/src/square.rs"]
-mod square;
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    loop {
+        clear_background(RED);
 
-fn main() {
-    #[allow(unused_imports)]
-    use glium::{glutin, Surface};
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
 
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
-    let positions = glium::VertexBuffer::new(&display, &square::VERTICES).unwrap();
-    let normals = glium::VertexBuffer::new(&display, &square::NORMALS).unwrap();
-    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
-                                          &square::INDICES).unwrap();
-
-    let vertex_shader_src = r#"
-        #version 140
-        in vec3 position;
-        in vec3 normal;
-        uniform mat4 matrix;
-        void main() {
-            gl_Position = matrix * vec4(position, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-        out vec4 color;
-        void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    "#;
-
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src,
-                                              None).unwrap();
-
-    event_loop.run(move |event, _, control_flow| {
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-        match event {
-            glutin::event::Event::WindowEvent { event, .. } => match event {
-                glutin::event::WindowEvent::CloseRequested => {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit;
-                    return;
-                },
-                _ => return,
-            },
-            glutin::event::Event::NewEvents(cause) => match cause {
-                glutin::event::StartCause::ResumeTimeReached { .. } => (),
-                glutin::event::StartCause::Init => (),
-                _ => return,
-            },
-            _ => return,
-        }
-
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
-
-        let matrix = [
-            [0.01, 0.0, 0.0, 0.0],
-            [0.0, 0.01, 0.0, 0.0],
-            [0.0, 0.0, 0.01, 0.0],
-            [0.0, 0.0, 0.0, 1.0f32]
-        ];
-
-        target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix },
-                    &Default::default()).unwrap();
-        target.finish().unwrap();
-    });
+        next_frame().await
+    }
 }
-
